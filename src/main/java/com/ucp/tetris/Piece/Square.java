1adf48eed35d38ac91442d93c6f65172829512bd
Aylen Meza Chiesa <130184706+aylenchiesa@users.noreply.github.com>
2025-09-14 01:18:50 +0000

    intento rotar square

--- src/main/java/com/ucp/tetris/Piece/Square.java
+++ src/main/java/com/ucp/tetris/Piece/Square.java
@@ -11,26 +11,9 @@ public class Square {
       return symbol + symbol + "\n" + symbol + symbol;
     }
 
-    // Puedes agregar este método en tu clase de pieza
+    // metodorotar
 
   private String rotate90(String shape) {
-      String[] rows = shape.split("\n");
-      int n = rows.length;
-      int m = rows[0].length();
-      char[][] matrix = new char[n][m];
-      for (int i = 0; i < n; i++) {
-          matrix[i] = rows[i].toCharArray();
+      
       }
-      char[][] rotated = new char[m][n];
-      for (int i = 0; i < n; i++) {
-          for (int j = 0; j < m; j++) {
-              rotated[j][n - 1 - i] = matrix[i][j];
-          }
-      }
-      StringBuilder sb = new StringBuilder();
-      for (char[] row : rotated) {
-          sb.append(new String(row)).append("\n");
-      }
-      return sb.toString().trim();
-  }
-}
+      
