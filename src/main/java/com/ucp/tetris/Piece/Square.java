38d3c624397b033e2443d69492cd2ad2440549ef
aylen <aylenchiesaok@gmail.com>
2025-09-15 20:23:33 -0300

    rotar cuadrado

--- src/main/java/com/ucp/tetris/Piece/Square.java
+++ src/main/java/com/ucp/tetris/Piece/Square.java
@@ -1,19 +1,34 @@
 package com.ucp.tetris.Piece;
 
 public class Square {
-    private String symbol;
+  private String symbol;
 
-    public Square(String symbol) {
-        this.symbol = symbol;
-    }
+  public Square(String symbol) {
+    this.symbol = symbol;
+  }
 
-    public String getCuadrado() {
-      return symbol + symbol + "\n" + symbol + symbol;
-    }
+  public String getCuadrado() {
+    return symbol + symbol + "\n" +
+        symbol + symbol;
+  }
 
-    // metodorotar
+  // rotar 90 grados no cambia la forma del cuadrado
+  public String rotar() {
+    return getCuadrado();
+  }
 
-  private String rotate90(String shape) {
-      
-      }
-      
+  //rotar 180 grados no cambia la forma del cuadrado
+  public String rotar180() {
+    return getCuadrado();
+  }
+
+  //rotar 270 grados no cambia la forma del cuadrado
+  public String rotar270() {
+    return getCuadrado();
+  }
+  
+  //rotar 360 grados no cambia la forma del cuadrado
+  public String rotar360() {
+    return getCuadrado();
+  }
+}
