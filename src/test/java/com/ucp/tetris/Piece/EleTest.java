87e91a89a9c8d6f29037f7e412a737099926a7b7
lau184 <agostinacod@gmail.com>
2025-09-16 13:46:48 -0300

    l

--- src/test/java/com/ucp/tetris/Piece/EleTest.java
+++ src/test/java/com/ucp/tetris/Piece/EleTest.java
@@ -1,1 +1,15 @@
-package com.ucp.tetris.Piece;
+package com.ucp.tetris;
+import main.java.com.ucp.tetris.Piece;
+
+import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert.assertTrue;
+
+public class EleTest {
+
+    @Test
+    public void CrearPiezaItest() {
+        Ele n = new Ele("L");
+        assertNotNull(pieza);
+        assertEquals("L", pieza.getForma());
+    }
+}
