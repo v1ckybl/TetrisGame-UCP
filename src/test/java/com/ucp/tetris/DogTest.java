fbf968338caf5ee49a2315dac3070cfc8ca799bb
lau184 <agostinacod@gmail.com>
2025-09-16 07:31:25 -0300

    cree test dog

---
+++ src/test/java/com/ucp/tetris/DogTest.java
@@ -0,0 +1,14 @@
+package com.ucp.tetris;
+
+import com.ucp.tetris.Piece.Dog;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DogTest {
+
+    @Test
+    public void testDogCreation() {
+        Dog dog = new Dog("X");
+        assertNotNull(dog);
+        assertEquals("X", dog.getSymbol());
+    }
