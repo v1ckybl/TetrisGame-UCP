8b5c5c0ea3105aebe5377466d4cefeffab9b1f51
Aylen Meza Chiesa <130184706+aylenchiesa@users.noreply.github.com>
2025-09-17 20:42:25 +0000

    cree ele invertida

---
+++ src/main/java/com/ucp/tetris/Piece/EleLeft.java
@@ -0,0 +1,20 @@
+package com.ucp.tetris.Piece;
+
+public class Ele {
+    private String symbol;
+
+    public Ele(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getEle() {
+      return " " + symbol + "\n" +
+             " " + symbol + "\n" +
+             symbol + symbol;
+    }
+    
+    public void rotarEle90() {
+          //return COMO ES LA ELE ROTADA???????;
+  }
+
+}
