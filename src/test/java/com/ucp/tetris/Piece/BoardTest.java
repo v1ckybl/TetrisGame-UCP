36381b0b7fa99de7cf79edb6afc051ac1eb3244b
lau184 <agostinacod@gmail.com>
2025-09-18 09:01:56 -0300

    test de board

--- src/test/java/com/ucp/tetris/Piece/BoardTest.java
+++ src/test/java/com/ucp/tetris/Piece/BoardTest.java
@@ -1,14 +1,16 @@
 package com.ucp.tetris.Piece;
-import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.ucp.tetris.Board;
 
 public class BoardTest {
-
     @Test
-    public 
-
-
+    public void dimensionesCorrectas() {
+        Board b = new Board();
+        assertEquals(10, b.getFila());
+        assertEquals(20, b.getColumna());
+    }
 }
 
 
