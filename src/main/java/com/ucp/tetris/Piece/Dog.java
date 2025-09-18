4223ad44c64fc6530272764934123b110886711c
aylen <aylenchiesaok@gmail.com>
2025-09-18 20:39:44 -0300

    fue arraylist

--- src/main/java/com/ucp/tetris/Piece/Dog.java
+++ src/main/java/com/ucp/tetris/Piece/Dog.java
@@ -1,30 +1,23 @@
 package com.ucp.tetris.Piece;
 
-public class Dog {
-    private String symbol;
+public class Dog extends PieceBase {
 
-    public Dog(String symbol) {
-        this.symbol = symbol;
+  public Dog(String symbol) {
+        setShape (new ArrayList<>(List.of(
+            List.of(null, symbol),
+            List.of(null, symbol),
+            List.of(symbol, symbol))));
     }
 
-    public String getShapeString() {    // Representación en forma de S
-        return " " + symbol + symbol + "\n"
-         + symbol + symbol + " ";
+  public String getEleLeft() {
+        StringBuilder sb = new StringBuilder();
+        for (List<String> row : getShape()) {
+            for (String cell : row) {
+                sb.append(cell != null ? cell : " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public String rotateRight() {    // Rotación simple (cambia la orientación de S a Z)
-        return symbol + "\n" +
-               symbol + symbol + "\n" +
-               " " + symbol;
-    }
-
-    public String rotateLeft() {
-        return " " + symbol + "\n" +
-               symbol + symbol + "\n" +
-               symbol;
-    }
 }
