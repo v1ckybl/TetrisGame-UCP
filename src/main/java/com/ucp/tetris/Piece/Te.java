40df92b7062e000ebaee53106e332642f0df4b06
aylen <aylenchiesaok@gmail.com>
2025-09-18 18:44:46 -0300

    arreglo rotar

--- src/main/java/com/ucp/tetris/Piece/Te.java
+++ src/main/java/com/ucp/tetris/Piece/Te.java
@@ -1,16 +1,36 @@
 package com.ucp.tetris.Piece;
 
-public class Te {
-    private String symbol;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Te(String symbol) {
-        this.symbol = symbol;
-    }
+public class Te extends PieceBase {
+  
+  private String symbol;
+  private List<List<String>> shape;
 
-    public String getShapeString() {
-      
-        return symbol + symbol + symbol + "\n" +
-               " " + symbol + " ";
+  public Te(String symbol) {
+    super(symbol, new ArrayList<>());
+    this.symbol = symbol;
+    this.shape = new ArrayList<>(List.of(
+        List.of(symbol, symbol, symbol),
+        List.of(null, symbol, null)));
+  }
+
+  public String getTe() {
+    StringBuilder sb = new StringBuilder();
+    for (List<String> row : shape) {
+      for (String cell : row) {
+        sb.append(cell != null ? cell : " "); //Si la celda no es null, agrega el symbol a la cadena.
+      }
+      sb.append("\n");
     }
+    return sb.toString();
+  }
+
+  // llamar el metodo para rotar de la interfaz Irotar que implementa PieceBase
+   public void rotateRight() {
+    super.rotateRight();
+    // Lógica adicional si es necesario
+
 }
 
