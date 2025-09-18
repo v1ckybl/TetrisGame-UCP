bb0f4f3b8a0c6300f06bdd36b9513fad28600fe5
aylen <aylenchiesaok@gmail.com>
2025-09-18 20:43:18 -0300

    dogs

--- src/main/java/com/ucp/tetris/Piece/Dog.java
+++ src/main/java/com/ucp/tetris/Piece/Dog.java
@@ -1,15 +1,15 @@
 package com.ucp.tetris.Piece;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Dog extends PieceBase {
 
   public Dog(String symbol) {
         setShape (new ArrayList<>(List.of(
-            List.of(null, symbol),
-            List.of(null, symbol),
-            List.of(symbol, symbol))));
+            List.of(null, symbol, symbol),
+            List.of(symbol, symbol, null))));
     }
 
-  public String getEleLeft() {
+  public String getDog() {
         StringBuilder sb = new StringBuilder();
         for (List<String> row : getShape()) {
             for (String cell : row) {
--- src/main/java/com/ucp/tetris/Piece/DogLeft.java
+++ src/main/java/com/ucp/tetris/Piece/DogLeft.java
@@ -1,19 +1,23 @@
 package com.ucp.tetris.Piece;
+import java.util.ArrayList;
+import java.util.List;
+    public class DogLeft extends PieceBase {
 
-    public class DogLeft {
-        private String symbol;
-
-        public DogLeft(String symbol) {
-            this.symbol = symbol;
-        }
-
-        public String getShapeString() {    // Representación en forma de S
-            return symbol + symbol + "\n" + " " + symbol + symbol;
-        }
-
-        public String getSymbol() {
-            return symbol;
-        }
-
+  public DogLeft(String symbol) {
+        setShape (new ArrayList<>(List.of(
+            List.of(symbol, symbol, null),
+            List.of(null, symbol, symbol))));
     }
 
+  public String getDog() {
+        StringBuilder sb = new StringBuilder();
+        for (List<String> row : getShape()) {
+            for (String cell : row) {
+                sb.append(cell != null ? cell : " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+}
