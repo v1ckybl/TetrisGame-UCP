4bbc1e668ba321a453e99dcc43385eecbd695d77
aylen <aylenchiesaok@gmail.com>
2025-09-19 01:53:22 -0300

    -

--- src/test/java/com/ucp/tetris/Piece/BoardTest.java
+++ src/test/java/com/ucp/tetris/Piece/BoardTest.java
@@ -11,6 +11,25 @@ public class BoardTest {
         assertEquals(10, b.getFila());
         assertEquals(20, b.getColumna());
     }
+
+
+@Test
+    public void testPieceFallsDownOneStepPerTick() throws NoSuchFieldException, IllegalAccessException {
+        Board board = new Board();
+        board.spawnNewPiece(); // La pieza se crea en (0, 4) por defecto
+        
+        // Usamos Reflection para acceder a la posición privada de la pieza en el tablero
+        // Esto se hace para fines de testing, ya que la posición no es pública
+        Field pieceYField = Board.class.getDeclaredField("pieceY");
+        pieceYField.setAccessible(true);
+        int initialY = pieceYField.getInt(board);
+
+        // Simulamos un tick, que debería mover la pieza una fila hacia abajo
+        board.moveCurrentPieceDown();
+        
+        int afterMoveY = pieceYField.getInt(board);
+
+        assertEquals(initialY + 1, afterMoveY, "La pieza debe bajar una posición por cada tick.");
+    }
+
 }
-
-
