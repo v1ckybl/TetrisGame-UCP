f43f1d3f787deee9c83b2798d2eaba52eef8d7d5
aylen <aylenchiesaok@gmail.com>
2025-09-18 21:32:03 -0300

    -

--- src/test/java/com/ucp/tetris/Piece/EleTest.java
+++ src/test/java/com/ucp/tetris/Piece/EleTest.java
@@ -35,42 +35,4 @@ public void testEleShapeString() {
     }
     
 
-
-    @Test
-    public void rotarDerecha() {
-        Ele l = new Ele("L");
-
-        l.rotateRight();
-        List<List<String>> actual = l.getShape();
-
-        List<List<String>> esperado = List.of(
-            List.of(null, null, "L"),
-            List.of("L", "L", "L")
-        );
-
-        assertTrue(formasIguales(esperado, actual));
-    }
-
-    private boolean formasIguales(List<List<String>> a, List<List<String>> b) {
-    if (a.size() != b.size()) return false;
-
-    for (int i = 0; i < a.size(); i++) {
-        List<String> filaA = a.get(i);
-        List<String> filaB = b.get(i);
-
-        if (filaA.size() != filaB.size()) return false;
-
-        for (int j = 0; j < filaA.size(); j++) {
-            String celdaA = filaA.get(j);
-            String celdaB = filaB.get(j);
-
-            if (celdaA == null && celdaB != null) return false;
-            if (celdaA != null && !celdaA.equals(celdaB)) return false;
-        }
-    }
-
-    return true;
-}
-
-
 }
