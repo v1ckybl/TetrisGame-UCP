50ebff9e77589cf9c7c2464e8cac2ac89ee11f68
v1ckybl <victoriabladilo@gmail.com>
2025-09-19 01:06:15 +0000

    rotate

--- src/main/java/com/ucp/tetris/Piece/Dog.java
+++ src/main/java/com/ucp/tetris/Piece/Dog.java
@@ -1,5 +1,6 @@
 package com.ucp.tetris.Piece;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class Dog extends PieceBase {
 
--- src/test/java/com/ucp/tetris/Piece/DogTest.java
+++ src/test/java/com/ucp/tetris/Piece/DogTest.java
@@ -1,9 +1,28 @@
 package com.ucp.tetris.Piece;
-import static org.junit.Assert.assertEquals; 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class DogTest {
 
+    @Test
+    public void testDogRotateRightOnce() {
+    Dog dog = new Dog("■");
+    dog.rotateRight();
+
+    String symbol = "■";
+    List<List<String>> expectedRotatedShape = new ArrayList<>(Arrays.asList(
+        Arrays.asList(symbol, null),
+        Arrays.asList(symbol, symbol),
+        Arrays.asList(null, symbol)
+    ));
+
+    assertEquals(expectedRotatedShape, dog.getShape());
 }
+
+
+}
+
