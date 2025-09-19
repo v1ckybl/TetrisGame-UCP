276ecc688237148f40d7b705cb50d8e92a5d4f9f
aylen <aylenchiesaok@gmail.com>
2025-09-19 09:23:30 -0300

    comentario sugerencia

--- src/test/java/com/ucp/tetris/Piece/BoardTest.java
+++ src/test/java/com/ucp/tetris/Piece/BoardTest.java
@@ -1,6 +1,8 @@
 package com.ucp.tetris.Piece;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.ucp.tetris.Board;
@@ -19,8 +21,11 @@ public class BoardTest {
     Ele ele = new Ele("■");
 
     // empieza en fila 0, columna 5
-    ele.setPosicion(0, 5);
+    ele.setPosicion(0, 5);  //la posicion la dbe setear EL TABLEROOOOOOO no la pieza
     board.setCurrentPiece(ele);
+
+    assertEquals(0, ele.getFila());
+    assertEquals(5, ele.getColumna());
 
     // ticks
     for (int j = 0; j < 20; j++) {
@@ -28,8 +33,9 @@ public class BoardTest {
     }
 
     assertEquals(17, ele.getFila());
-    assertEquals(5, ele.getColumna()); 
-}
+    assertEquals(5, ele.getColumna());
+    
+    }
 
 
   @Test
