47dd4a5557eb4dc53c41f289e5632a37e34935ce
v1ckybl <victoriabladilo@gmail.com>
2025-09-19 11:14:49 +0000

    colision

--- src/test/java/com/ucp/tetris/Piece/BoardTest.java
+++ src/test/java/com/ucp/tetris/Piece/BoardTest.java
@@ -31,26 +31,34 @@ public class BoardTest {
 }
 
     @Test
-    public void testEleChocaConOtraPieza() {
-        Board board = new Board();
+    public void testSquareCaeEncimaDeEle() {
+    Board board = new Board();
 
-        // Fijamos un cuadrado en el fondo en columna 5
-        Square bloqueFijo = new Square("■");
-        bloqueFijo.setPosicion(18, 5); // ocupa filas 18 y 19
-        board.fijarPieza(bloqueFijo);
+    // 1. Creamos una Ele y la dejamos caer hasta el fondo
+    Ele ele = new Ele("■");
+    ele.setPosicion(0, 5);
+    board.setCurrentPiece(ele);
 
-        // Creamos una Ele que empieza en fila 0, columna 5
-        Ele ele = new Ele("■");
-        ele.setPosicion(0, 5);
-        board.setCurrentPiece(ele);
+    for (int i = 0; i < 20; i++) {
+        board.tick(); // la Ele llega al fondo y se fija
+    }
 
-        // Hacemos caer la Ele simulando ticks
-        for (int i = 0; i < 20; i++) {
-            board.tick();
-        }
+    // Verificamos que la Ele quedó en fila 17
+    assertEquals(17, ele.getFila());
 
-        // La Ele debería detenerse en fila 15, justo encima del cuadrado
-        assertEquals(15, ele.getFila());
+    // 2. Ahora tiramos un Square encima en la misma columna
+    Square square = new Square("■");
+    square.setPosicion(0, 5);
+    board.setCurrentPiece(square);
+
+    for (int i = 0; i < 20; i++) {
+        board.tick();
+    }
+
+    // El Square debería caer y detenerse justo arriba de la Ele
+    // Como la Ele ocupa hasta fila 19, el Square (que mide 2 de alto)
+    // debe quedar con su fila inicial en 15
+    assertEquals(15, square.getFila());
 }
 
 
