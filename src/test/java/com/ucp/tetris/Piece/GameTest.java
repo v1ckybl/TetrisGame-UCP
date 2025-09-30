a0064d0770415cf43a29139f9d6a112f06875779
lau184 <agostinacod@gmail.com>
2025-09-30 13:14:42 -0300

    test

--- src/test/java/com/ucp/tetris/Piece/GameTest.java
+++ src/test/java/com/ucp/tetris/Piece/GameTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.ucp.tetris.Board;
+import com.ucp.tetris.Game;
 
 public class GameTest {
     
@@ -14,4 +15,11 @@ public class GameTest {
     assertEquals(20, b.getFila());
     assertEquals(10, b.getColumna());
   }
+
+  @Test
+    public void testConstructorNoException() {
+        Board board = new Board();
+        new Game(board);
+        // Si no lanza excepción, el constructor funciona
+    }
 }
