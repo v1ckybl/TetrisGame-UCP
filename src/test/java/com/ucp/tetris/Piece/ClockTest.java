eb736d1a5eff3ba4d8cd3379f77bf26b534d8174
v1ckybl <victoriabladilo@gmail.com>
2025-09-30 16:05:53 +0000

    .......

--- src/test/java/com/ucp/tetris/Piece/ClockTest.java
+++ src/test/java/com/ucp/tetris/Piece/ClockTest.java
@@ -1,6 +1,20 @@
 package com.ucp.tetris.Piece;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class ClockTest {
     
+     @Test
+    public void testTicksAcumulan() {
+        Runnable dummy = () -> {};
+        Clock clock = new Clock(5, dummy);
+
+        for (int i = 0; i < 3; i++) {
+            clock.tick();
+        }
+
+        assertEquals(3, clock.getTicks());
+    }
 }
