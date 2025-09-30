80fc94ef796d52930c45397d9de98bad7eacfc86
lau184 <agostinacod@gmail.com>
2025-09-30 12:56:36 -0300

    arreglos

--- src/main/java/com/ucp/tetris/Piece/DogLeft.java
+++ src/main/java/com/ucp/tetris/Piece/DogLeft.java
@@ -10,20 +10,4 @@ import java.util.List;
             Arrays.asList(null, symbol, symbol))));
     }
 
-  /*public String getDog() {
-        StringBuilder sb = new StringBuilder();
-    List<List<String>> shape = getShape();
-
-    for (int i = 0; i < shape.size(); i++) {
-      List<String> row = shape.get(i);
-      for (String cell : row) {
-        sb.append(cell != null ? cell : " ");
-      }
-      if (i < shape.size() - 1) { //que no haya un salto de linea mas 
-        sb.append("\n");
-      }
-    }
-    return sb.toString();
-  }*/
-
 }
